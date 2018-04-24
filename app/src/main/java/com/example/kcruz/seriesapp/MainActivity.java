39b3d8c94aa5ad9bb684b099b41095b13500be2a
00010216 <00010216@uca.edu.sv>
2018-04-23 22:22:21 -0600

    Asignar el nombre a las tabs y dandole la funcionalidad para desplazarse de tabs con el ViewPager

--- app/src/main/java/com/example/kcruz/seriesapp/MainActivity.java
+++ app/src/main/java/com/example/kcruz/seriesapp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.kcruz.seriesapp;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FavoritesFragment.OnFragmentInteractionListener, SeriesFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tablayout); //guardando la vista del tablayout en un objeto
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Series)); //asignandole el nombre respectivo al tab
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Favorites));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager)findViewById(R.id.pager);
+        final PagerAdapter pageAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        //dandole las funcionalidades a las tabs
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
 }
