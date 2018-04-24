9a36ee383c95c679637c0f4fa9862fc77ed7dc35
00010216 <00010216@uca.edu.sv>
2018-04-23 20:32:10 -0600

    Creacion de fragments, Page Adapter,creacion de ActionBar y ToolBar

---
+++ app/src/main/java/com/example/kcruz/seriesapp/PagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.kcruz.seriesapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int NumberOfTabs;
+
+    public PagerAdapter (FragmentManager fm, int NumberOfTabs){
+        super(fm);
+        this.NumberOfTabs = NumberOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0: FavoritesFragment tab1 = new FavoritesFragment(); return tab1;
+            case 1: SeriesFragment tab2 = new SeriesFragment(); return tab2;
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() { //envia el numero de tabs que se tiene
+        return NumberOfTabs;
+    }
+}
